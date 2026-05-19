fc41a0f4dd6042a20fd83f247504333cfd75a3d9
Ritika <18.ritika.ritz@gmail.com>
2026-05-19 23:17:13 +0530

    added color const and theme

---
+++ lib/config/theme/theme_config.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:offline_ai_tutor/core/utils/constants/color_consts.dart';
+
+class ThemeConfig {
+  static ThemeData appThemeData() {
+    return ThemeData(
+      useMaterial3: true,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: ColorConsts.primaryColor,
+        surface: ColorConsts.backgroundColor,
+      ),
+      // primaryColor: ColorConsts.primaryColor,
+      scaffoldBackgroundColor: ColorConsts.backgroundColor,
+      primaryTextTheme: primaryTextTheme,
+      textTheme: primaryTextTheme,
+      appBarTheme: AppBarTheme(
+        backgroundColor: ColorConsts.backgroundColor,
+        foregroundColor: ColorConsts.textPrimaryColor,
+        centerTitle: false,
+        titleTextStyle: GoogleFonts.syne(
+          color: ColorConsts.textPrimaryColor,
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          shape: WidgetStatePropertyAll(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadiusGeometry.all(Radius.circular(10)),
+            ),
+          ),
+
+          backgroundColor: WidgetStatePropertyAll(ColorConsts.primaryColor),
+
+          foregroundColor: WidgetStatePropertyAll(ColorConsts.textPrimaryColor),
+
+          textStyle: WidgetStatePropertyAll(
+            GoogleFonts.dmSans(
+              color: ColorConsts.textPrimaryColor,
+              fontSize: 13,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  static TextTheme get primaryTextTheme => TextTheme(
+    //splash screen logo style
+    displayLarge: GoogleFonts.syne(
+      color: ColorConsts.textPrimaryColor,
+      fontSize: 36,
+      fontWeight: FontWeight.bold,
+    ),
+
+    //muted text med
+    bodyMedium: GoogleFonts.dmSans(
+      color: ColorConsts.whiteColor35,
+      fontSize: 12,
+    ),
+
+    //muted text small
+    bodySmall: GoogleFonts.dmSans(
+      color: ColorConsts.whiteColor35,
+      fontSize: 10,
+    ),
+
+    //large titles
+    titleLarge: GoogleFonts.syne(
+      color: ColorConsts.textPrimaryColor,
+      fontSize: 20,
+      fontWeight: FontWeight.w500,
+    ),
+
+    titleMedium: GoogleFonts.syne(
+      color: ColorConsts.textPrimaryColor,
+      fontSize: 16,
+      fontWeight: FontWeight.w500,
+    ),
+
+    //small titles
+    titleSmall: GoogleFonts.dmSans(
+      color: ColorConsts.textPrimaryColor,
+      fontSize: 13,
+      fontWeight: FontWeight.w500,
+    ),
+  );
+}
---
+++ lib/core/utils/constants/color_consts.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/widgets.dart';
+
+class ColorConsts {
+  static const Color primaryColor = Color(0xffA695FF);
+  static const Color textPrimaryColor = Color(0xffF0EFF8);
+
+  static const Color backgroundColor = Color(0xff0A0A0F);
+  static const Color surfaceColor = Color(0xff18181F);
+  static const Color accentColor = Color(0xff4FC3A1);
+
+  static const Color successColor = Color(0xff3DCF8E);
+  static const Color warningColor = Color(0xffF5A623);
+  static const Color errorColor = Color(0xffFF5E5E);
+
+  static const Color blackColor = Color(0xff000000);
+  static const Color whiteColor = Color(0xffffffff);
+  static const Color whiteColor35 = Color(0x35ffffff);
+
+  static const Color buttonPLinearColor1 = Color(0xff7C6AFF);
+  static const Color buttonPLinearColor2 = Color(0xff9B8BFF);
+  static const Color buttonSecondaryColor = Color(0x107C6AFF);
+  static const Color buttonSecondaryStrokeColor = Color(0x307C6AFF);
+  static const Color buttonMutedColor = Color(0x04ffffff);
+  static const Color buttonMutedStrokeColor = Color(0x10ffffff);
+  static const Color buttonSuccessColor = Color(0x104FC3A1);
+  static const Color buttonSuccessStrokeColor = Color(0x204FC3A1);
+  static const Color buttonErrorColor = Color(0x10FF5E5E);
+  static const Color buttonErrorStrokeColor = Color(0x20FF5E5E);
+}
